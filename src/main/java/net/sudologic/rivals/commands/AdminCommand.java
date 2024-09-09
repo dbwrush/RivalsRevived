d7566e55e5c3efe4e15646125c0f5c3ea3ea80e6
David Rush <43554173+dbwrush@users.noreply.github.com>
2024-09-09 11:11:53 -0500

    Initial custom item support

--- src/main/java/net/sudologic/rivals/commands/AdminCommand.java
+++ src/main/java/net/sudologic/rivals/commands/AdminCommand.java
@@ -1,12 +1,23 @@
 package net.sudologic.rivals.commands;
 
 import net.sudologic.rivals.Rivals;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdminCommand implements CommandExecutor {
+    //data structure to match eye type to NBT data
+    private static final Map<String, ItemMeta> eyeTypes = new HashMap<String, ItemMeta>() {{
+    }};
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (commandSender instanceof Player) {
@@ -45,6 +56,29 @@ public class AdminCommand implements CommandExecutor {
                     commandSender.sendMessage("[Rivals] Failed to change setting " + settingName + ".");
                 }
                 return true;
+            case "eye":
+                // Give the sender an eye of ender with custom NBT data
+                if (!(commandSender instanceof Player player)) {
+                    commandSender.sendMessage("[Rivals] This command can only be run by a player.");
+                    return true;
+                }
+                if (args.length < 2) {
+                    commandSender.sendMessage("[Rivals] This subcommand requires an eye type");
+                    return true;
+                }
+                if (args[1].equals("help")) {
+                    showEyeTypes((Player) commandSender);
+                    return true;
+                }
+                // Custom NBT data for the Eye of Ender
+                ItemStack eye = new ItemStack(Material.ENDER_EYE);
+                ItemMeta data = eyeTypes.get(args[1]);
+                if (data == null) {
+                    commandSender.sendMessage("[Rivals] Unknown eye type. Use /rivalsadmin eye help for a list of eye types.");
+                    return true;
+                }
+                eye.setItemMeta(data);
+                player.getInventory().addItem(eye);
             case "help":
                 commandSender.sendMessage("[Rivals] Admin Command Help:\n" +
                         "- /rivalsadmin setMainShopRegion <id>: Set the main shop region.\n" +
@@ -56,6 +90,14 @@ public class AdminCommand implements CommandExecutor {
             default:
                 commandSender.sendMessage("[Rivals] Unknown subcommand. Use /rivalsadmin help for a list of commands.");
                 return true;
+        }
+    }
+
+    //show list of eye types to player
+    public void showEyeTypes(Player player) {
+        player.sendMessage("[Rivals] Eye Types:");
+        for (String type : eyeTypes.keySet()) {
+            player.sendMessage(type);
         }
     }
 }
